83a2f651c8bb0bf1113189ab6edc30a793ff4cb2
jean <tadros.jean@gmail.com>
2020-10-26 13:33:17 +0100

    add parsers
    Former-commit-id: a23b3235df4c26d8c70afeb0e39c0a001e30053a

--- second/cumu_test.py
+++ second/cumu_test.py
@@ -1,8 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+import os, time, argparse
 import astropy.io.fits as fits
 from scipy.spatial.distance import euclidean as euclid
+
+import matplotlib.widgets as widgets
+from matplotlib import ticker, cm
+import time
+
+arg_parser  = argparse.ArgumentParser()
+arg_parser.add_argument('-fn', '--file_name', required=True, help=' file name without extension')
+arg_parser.add_argument('-v', '--version', required=True, help=' file name without extension')
+arg_parser.add_argument('-hpw','--half_patch_width', required=False, help= 'half patch width')
+arg_parser.add_argument('-select', '--select', required=False, help='select source region')
+arg_parser.add_argument('-mode', '--mode', required=True, help=' rect or Otsu mode')
+arg_parser.add_argument('-size', '--size', required=False, help=' rect or Otsu mode')
+args = vars(arg_parser.parse_args())
+fn = args['file_name']
+v = args['version']
+mode = args['mode']
+s = args['size']
+
+image_path  = '/Users/jeantad/Desktop/new_crab/DATA_TEST/'
+path = image_path + fn + '.fits'
+
+dir = '/Users/jeantad/Desktop/new_crab/OUT_TEST/'+str(fn)+'/analysis/rect/'+str(s)+'/ROI_'+str(v)
+org  = fits.open(path)[1].data
+mask = np.load(dir + '/mask.npy')
+inp  = np.load(dir + '/inpainting/inpainted_image.npy')
+lin  = np.load(dir + '/linear/linear_interpol_image.npy')
+bil  = np.load(dir + '/bilinear/reconstruction.npy')
+
 
 def better_histogram(array):
     val = []
@@ -14,13 +42,30 @@ def better_histogram(array):
 
     return plt.hist(val, 100)
 
+for x in range(mask.shape[0]):
+    for y in range(mask.shape[1]):
+        if mask[x][y] == 255:
+            continue
+        else:
+            lin[x][y] = np.nan
+            inp[x][y] = np.nan
+            org[x][y] = np.nan
+            bil[x][y] = np.nan
+
+better_histogram(org)
+better_histogram(inp)
+#better_histogram(lin)
+better_histogram(bil)
+plt.show()
+exit()
+
 def _distance(x, y):
     return euclid(x, y)
 
-result = {}
-save_result = {}
 
-for fn in ['500']:
+save_result = {}
+result = {}
+for fn in ['500', '350']:
     result[fn] = {}
     save_result[fn] = {}
 
@@ -38,6 +83,9 @@ for fn in ['500']:
             inp  = np.load(dir + '/inpainting/inpainted_image.npy')
             lin  = np.load(dir + '/linear/linear_interpol_image.npy')
             bil  = np.load(dir + '/bilinear/reconstruction.npy')
+
+            org_h = better_histogram(org)
+            plt.show()
 
             for x in range(mask.shape[0]):
                 for y in range(mask.shape[1]):
@@ -68,7 +116,6 @@ for fn in ['500']:
     inp_e = [result[fn][elem]['inp'][1] for elem in range(0,4)]
     bil_e = [result[fn][elem]['bil'][1] for elem in range(0,4)]
 
-    del result
     save_result[fn]['lin'] = [lin_v, lin_e]
     save_result[fn]['inp'] = [inp_v, inp_e]
     save_result[fn]['bil'] = [bil_v, bil_e]
