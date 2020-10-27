0e085e4be59cbaa0cf73bac53674e6cfd2e929b7
jean <tadros.jean@gmail.com>
2020-10-27 16:00:58 +0100

    8 to 16 bits images

--- core/linearizer.py
+++ core/linearizer.py
@@ -59,7 +59,6 @@ class Interpol(object):
             end  = time.time()
             cv2.imwrite(output_path+'/updatedMask/updatedMask_%.2d.png'%k, self.updatedMask)
             cv2.imwrite(output_path+'/iterations/inpaintedImage_%.2d.png'%k, self.workImage)
-            
             print 'Iteration '+str(k)+' -- '+str(np.round(end-start,2))+' sec'
 
         self.result = np.copy(self.workImage)
