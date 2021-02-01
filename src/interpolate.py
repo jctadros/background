b763976e5ed74da9dbe6d8345277a3a738751fed
Jean <tedros.jean@gmail.com>
2021-02-01 15:20:33 +0100

    clearning and removal of rect mode;
    Former-commit-id: 84445653e0f4acb611e94c1b0ed0ca5126945af9

--- src/interpolate.py
+++ src/interpolate.py
@@ -1,48 +1,31 @@
-import os, sys, argparse, math
 import numpy as np
+import os, argparse, math
 import matplotlib.pyplot as plt
-from linearizer import Interpol
-from matplotlib import ticker, cm
+from matplotlib import cm 
+
+from linearizer import Interpolator
 
 arg_parser  = argparse.ArgumentParser()
 arg_parser.add_argument('-fn', '--file_name', required=True, help=' file name without extension')
-arg_parser.add_argument('-v', '--version', required=True, help=' file name without extension')
-arg_parser.add_argument('-mode', '--mode', required=True, help=' rect or Otsu mode')
-arg_parser.add_argument("-size", "--size", required=False, help=" size of rect")
+arg_parser.add_argument('-v', '--version', required=True, help=' version of ROI')
 
 args = vars(arg_parser.parse_args())
 file_name = args['file_name']
 version = int(args['version'])
-mode = args['mode']
-if mode == 'rect':
-    size = args['size']
 
-output_path = '/Users/jeantad/Desktop/new_crab/OUT_TEST/'
-directory_1 = output_path + str(file_name) + '/Otsu'
-directory_2 = output_path + str(file_name) + '/analysis/otsu/ROI_' + str(version)
-directory_4 = output_path + str(file_name) + '/analysis/rect/ROI_' + str(version)
+output_path = '/Users/jeantad/Desktop/new_crab/OUT_TEST/' 
+directory_1 = output_path + file_name + '/ROI_' + str(version)
+directory_2 = directory_1 + '/linear'
+directory_3 = directory_2 + '/iterations'
+directory_4 = directory_2 + '/updatedMask'
 
-if mode == 'rect':
-    directory_3 = output_path + str(file_name) + '/analysis/rect/'+str(size)+'/ROI_'+str(version)
-    image_path  = directory_3 + '/masked_image.npy'
-    mask_path   = directory_3 + '/mask.npy'
-    output_path = directory_3 + '/linear'
-    coord_path  = directory_3 + '/contour_coord.npy'
-    directory_4 = output_path + '/iterations'
-    directory_5 = output_path + '/updatedMask'
+image_path  = directory_1 + '/masked_image.npy'
+mask_path   = directory_1 +'/mask_image.npy'
+coord_path  = directory_1 +'/contour_coord.npy'
 
-if mode == 'otsu':
-    image_path  = directory_2 + '/masked_image.npy'
-    mask_path   = directory_2 +'/mask.npy'
-    output_path = directory_2 +'/linear'
-    coord_path  = directory_2 +'/contour_coord.npy'
-    directory_4 = output_path + '/iterations'
-    directory_5 = output_path + '/updatedMask'
-
-for dir in [directory_4, directory_5]:
+for dir in [directory_3, directory_4]:
     if not os.path.exists(dir):
         os.makedirs(dir)
-
 try:
     originalImage = np.load(image_path)
     mask = np.load(mask_path)
@@ -51,11 +34,11 @@ except:
     print('Error: file not available')
     exit(1)
 
-cx = int(np.mean(xcoord))
-cy = int(np.mean(ycoord))
-i = Interpol(originalImage, mask, None)
-i.Linear(file_name, output_path)
-result = i.result
+cx, cy = int(np.mean(xcoord)), int(np.mean(ycoord))
+
+model = Interpolator(originalImage, mask, None)
+model.linear_interpolator(file_name, directory_2)
+result = model.result
 
 float_result = (result*(np.nanmax(originalImage)-np.nanmin(originalImage)))/(2**16 - 1)+np.nanmin(originalImage)
 mean, std = np.nanmean(float_result), np.nanstd(float_result)
@@ -69,34 +52,27 @@ f = plt.figure()
 ax = f.add_subplot(111)
 ax.axis('off')
 sh = 100
-if mode == 'rect':
-    if version == 0:
-        zoom_float = float_result[cx-sh:cx+sh, cy-sh:cy+sh]
-    else:
-        zoom_float = float_result[cy-sh:cy+sh, cx-sh:cx+sh]
+if version == 0:
+  co = np.load(coord_path)
+  xc , yc = co[0], co[1]
+  [x, y] = [[0,float_result.shape[0]], [float_result.shape[1], 0]]
+  xm, ym = np.mean(xc), np.mean(yc)
+  center_x = int(np.mean(xc-xm+float(x[0]+x[1])/2))
+  center_y = int(np.mean(yc-ym+float(y[0]+y[1])/2))
 
-if mode == 'otsu':
-    if version == 0:
-        co = np.load(coord_path)
-        xc , yc = co[0], co[1]
-        [x, y] = [[0,float_result.shape[0]], [float_result.shape[1], 0]]
-        xm, ym = np.mean(xc), np.mean(yc)
-        center_x = int(np.mean(xc-xm+float(x[0]+x[1])/2))
-        center_y = int(np.mean(yc-ym+float(y[0]+y[1])/2))
+  zoom_float = float_result[center_x-sh:center_x+sh+1, center_y-sh:center_y+sh+1]
 
-        zoom_float = float_result[center_x-sh:center_x+sh+1, center_y-sh:center_y+sh+1]
+else:
+  zoom_float = float_result[cy-sh:cy+sh, cx-sh:cx+sh]
 
-    else:
-        zoom_float = float_result[cy-sh:cy+sh, cx-sh:cx+sh]
-
-    mu, std = np.nanmedian(zoom_float), np.nanstd(zoom_float)
-    for x in range(zoom_float.shape[0]):
-        for y in range(zoom_float.shape[1]):
-            if np.absolute(zoom_float[x][y]-mu) >= 3*std:
-                zoom_float[x][y] = np.nan
+  mu, std = np.nanmedian(zoom_float), np.nanstd(zoom_float)
+  for x in range(zoom_float.shape[0]):
+     for y in range(zoom_float.shape[1]):
+         if np.absolute(zoom_float[x][y]-mu) >= 3*std:
+             zoom_float[x][y] = np.nan
 
 plt.imshow(zoom_float, cmap=cm.seismic)
 plt.tight_layout()
-plt.savefig(output_path+'/linear_interpol_image.png')
+plt.savefig(directory_2 + '/linear_interpol_image.png')
 plt.close(f)
-np.save(output_path+'/linear_interpol_image.npy', float_result)
+np.save(directory_2 + '/linear_interpol_image.npy', float_result)
